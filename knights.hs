bf97c660731856611d949388b28828c7baabd331
stanly <stanlyjohn2@gmail.com>
2015-11-03 23:45:38 +0530

    Implement basic heuristics

--- knights.hs
+++ knights.hs
@@ -21,10 +21,20 @@ mk_board r c = mkGraph (zip [1 .. r*c] (repeat ()))
                        (map (\(i, j) -> (i, j, ()))
                             (get_edges r c))
 
-next_hop g ns = foldr (get_min g) Nothing ns
-
 get_min g n (Just (d, ns)) | outdeg g n == d = Just (d, n:ns)
                            | outdeg g n < d = Just (outdeg g n, [n])
                            | otherwise = Just (d, ns)
 get_min g n Nothing = Just (outdeg g n, [n])
 
+select_hop g ns = foldr (get_min g) Nothing ns
+
+next_hop g n = let (c, g') = match n g
+                   Just (_, _, _, out_gns) = c
+                   out_ns = (map (\(_, n') -> n') out_gns)
+               in case select_hop g' out_ns of
+                   Just (_, min_hops) -> (g', Just (head min_hops))
+                   Nothing -> (g', Nothing)
+
+hop_seq g n = case next_hop g n of
+              (g', Just n') -> n:hop_seq g' n'
+              (g', Nothing) -> [n]
